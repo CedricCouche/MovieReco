f0f1b548972198243eb0ed66a043ea6564a5bbd6
CedricCouche <cedric.couche@pm.me>
2023-11-09 15:54:50 +0000

    clean-up container streamlit

--- container_streamlit/3_Movie-Recommandation.py
+++ container_streamlit/3_Movie-Recommandation.py
@@ -11,19 +11,19 @@ target_tconst = st.text_input('Movie tconst :', 'tt0001614')
 st.markdown('## Cosine Similarity TOP 10 recommandation')
 
 
-def get_reco_cs(tconst):
+# def get_reco_cs(tconst):
 
-    endpoint = 'http://container_api:8000/get_reco_cs/{}'.format(str(tconst))
-    response = requests.get(endpoint).json()
+#     endpoint = 'http://container_api:8000/get_reco_cs/{}'.format(str(tconst))
+#     response = requests.get(endpoint).json()
 
-    return response
+#     return response
 
-def get_reco_cs2(tconst):
+# def get_reco_cs2(tconst):
 
-    endpoint = 'http://container_api:8000/get_reco_cs/{}'.format(tconst)
-    response = requests.get(endpoint).json()
+#     endpoint = 'http://container_api:8000/get_reco_cs/{}'.format(tconst)
+#     response = requests.get(endpoint).json()
 
-    return response
+#     return response
 
 def get_reco_cs3(tconst):
 
@@ -34,15 +34,7 @@ def get_reco_cs3(tconst):
 
 # http://localhost:8000/get_reco_cs/%22tt0001614%22
 
-if st.button('Get Reco'):
-    result = get_reco_cs(target_tconst)
-    st.write('result: %s' % result)
-
-if st.button('Get Reco2'):
-    result = get_reco_cs2(target_tconst)
-    st.write('result: %s' % result)
-
-if st.button('Get Reco3'):
+if st.button('Get Recommandation using Cosine Similarity'):
     result = get_reco_cs3(target_tconst)
     st.write('result: %s' % result)
 
